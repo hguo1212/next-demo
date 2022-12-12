690bd31a87aaabd1c013f46b4704ce461cbc45fb
hguo1212 <hguo1212@163.com>
2022-12-12 16:20:19 +0800

    更新文章State Hook

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Date from '../components/date';
-import { getSortedPostsData } from '../lib/post';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Date from "../components/date";
+import { getSortedPostsData } from "../lib/post";
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Hannah. I'm a fonted-web developer. You can contact me on GitHub.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          Hello, I'm Hannah. I'm a fonted-web developer. You can contact me on
+          GitHub.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -26,7 +29,7 @@ export default function Home({allPostsData}) {
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-              <Date dateString={date} />
+                <Date dateString={date} />
               </small>
             </li>
           ))}
@@ -36,10 +39,9 @@ export default function Home({allPostsData}) {
   );
 }
 
-
-export async function getStaticProps () {
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
-    props: {allPostsData},
-  }
+    props: { allPostsData },
+  };
 }
--- pages/users.js
+++ pages/users.js
@@ -1,110 +1,129 @@
-import { useState } from 'react';
-import useSWR, { SWRConfig, useSWRConfig } from 'swr';
-import { prop, map, reject, eq, propEq } from 'lodash/fp';
-import Image from 'next/image';
-import Layout from "../components/layout"
-import style from './user.module.css'
-import { del,post, read } from '../hooks/request'
+import { useState } from "react";
+import useSWR, { SWRConfig, useSWRConfig } from "swr";
+import { prop, map, reject, eq, propEq } from "lodash/fp";
+import Image from "next/image";
+import Layout from "../components/layout";
+import style from "./user.module.css";
+import { del, post, read } from "../hooks/request";
 
-
-const fetcher = async url => {
-  const res = await fetch(url)
+const fetcher = async (url) => {
+  const res = await fetch(url);
   return res.json();
-}
+};
 
-export default function User({fallback}) {
-  const [inputVal, setInputVal] = useState('');
-  const [cnt, setCnt] = useState(0)
+export default function User({ fallback }) {
+  const [inputVal, setInputVal] = useState("");
+  const [cnt, setCnt] = useState(0);
 
-  console.log('cnt==> User', cnt)
-  const {data, error} = useSWR(["http://localhost:3000/api/users", cnt], fetcher)
+  console.log("cnt==> User", cnt);
+  const { data, error } = useSWR(
+    ["http://localhost:3000/api/users", cnt],
+    fetcher
+  );
 
-  return <Layout>
+  return (
+    <Layout>
       <article>
         {!error && !data && <div>loading</div>}
         {error && <div>Error fetching data</div>}
-        {
-          map((item)=> {
-            return <div className={style.item} key={prop('_id')(item)}>{item.name} 
-                  <Image
-                  priority
-                  src="/icons/delete.svg"
-                  className={style.deleteIcon}
-                  height={30}
-                  width={30}
-                  alt=""
-                  onClick={async ()=>{
-                   const xx =  await del('http://localhost:3000/api/users', {id: prop('_id')(item)}).then((res)=> {
-                      console.log('del==>>res',res)
-                      setCnt(val => val-1)
-                    })
-                    console.log('del del del===>',xx)
-                  }} />
-              </div>
-            })(data)
-        }
+        {map((item) => {
+          return (
+            <div className={style.item} key={prop("_id")(item)}>
+              {item.name}
+              <Image
+                priority
+                src="/icons/delete.svg"
+                className={style.deleteIcon}
+                height={30}
+                width={30}
+                alt=""
+                onClick={async () => {
+                  const xx = await del("http://localhost:3000/api/users", {
+                    id: prop("_id")(item),
+                  }).then((res) => {
+                    console.log("del==>>res", res);
+                    setCnt((val) => val - 1);
+                  });
+                  console.log("del del del===>", xx);
+                }}
+              />
+            </div>
+          );
+        })(data)}
       </article>
       <div className={style.flex}>
-        <input value={inputVal} onChange={(e)=> setInputVal(e.target.value)}/>
+        <input value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
         <Image
           priority
           src="/icons/add.svg"
           height={30}
           width={30}
           alt=""
-          onClick={()=>{
-            post('http://localhost:3000/api/users',{inputVal}).then((res)=>{
-              console.log('res',res)
-              setInputVal('')
-              console.log('ssssss')
-              setCnt(val => val+1)
-            })
-            }}
-          />
+          onClick={() => {
+            post("http://localhost:3000/api/users", { inputVal }).then(
+              (res) => {
+                console.log("res", res);
+                setInputVal("");
+                console.log("ssssss");
+                setCnt((val) => val + 1);
+              }
+            );
+          }}
+        />
       </div>
-  </Layout>
+    </Layout>
+  );
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const res = await fetch("http://localhost:3000/api/users");
   const users = await res.json();
 
   return {
     props: {
       fallback: {
-        '/api/users': users,
-      }
+        "/api/users": users,
+      },
     },
-  }
+  };
 }
 
-function Users({cnt, setCnt}) {
-  console.log('cnt',cnt)
-  const {data, error} = useSWR(["http://localhost:3000/api/users", cnt], fetcher)
-  if(error) {
-    return <div>Error fetching data</div>
+function Users({ cnt, setCnt }) {
+  console.log("cnt", cnt);
+  const { data, error } = useSWR(
+    ["http://localhost:3000/api/users", cnt],
+    fetcher
+  );
+  if (error) {
+    return <div>Error fetching data</div>;
   }
 
-  if(!error && !data) return <div>loading</div>
+  if (!error && !data) return <div>loading</div>;
 
-  return<article>
-  {
-    map((item)=> {
-      return <div className={style.item} key={prop('_id')(item)}>{item.name} 
+  return (
+    <article>
+      {map((item) => {
+        return (
+          <div className={style.item} key={prop("_id")(item)}>
+            {item.name}
             <Image
-            priority
-            src="/icons/delete.svg"
-            className={style.deleteIcon}
-            height={30}
-            width={30}
-            alt=""
-            onClick={()=>{
-              del('http://localhost:3000/api/users', {id: prop('_id')(item)}).then((res)=> {
-                setCnt(val=> val -1)
-              })
-            }} />
-        </div>
-    })(data)
-  }
-</article>
+              priority
+              src="/icons/delete.svg"
+              className={style.deleteIcon}
+              height={30}
+              width={30}
+              alt=""
+              onClick={() => {
+                del("http://localhost:3000/api/users", {
+                  id: prop("_id")(item),
+                }).then((res) => {
+                  setCnt((val) => val - 1);
+                });
+              }}
+            />
+          </div>
+        );
+      })(data)}
+    </article>
+  );
 }
