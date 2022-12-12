3ac1000f94b4ba720ad3c9d58f3ff42fabe5418c
hguo1212 <hguo1212@163.com>
2022-12-12 18:27:27 +0800

    add production config

--- pages/users.js
+++ pages/users.js
@@ -1,133 +1,90 @@
-export default function () {
-  return <div>users</div>
+import { useState } from "react";
+import useSWR, { SWRConfig, useSWRConfig } from "swr";
+import { prop, map, reject, eq, propEq } from "lodash/fp";
+import Image from "next/image";
+import Layout from "../components/layout";
+import style from "./user.module.css";
+import { del, post, read } from "../hooks/request";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
+export default function User({baseUrl, fallback }) {
+  const [inputVal, setInputVal] = useState("");
+  const [cnt, setCnt] = useState(0);
+
+
+  console.log("cnt==> User", cnt);
+  const { data, error } = useSWR(
+    [`${baseUrl}/api/users`, cnt],
+    fetcher
+  );
+
+  return (
+    <Layout>
+      <article>
+        {!error && !data && <div>loading</div>}
+        {error && <div>Error fetching data</div>}
+        {map((item) => {
+          return (
+            <div className={style.item} key={prop("_id")(item)}>
+              {item.name}
+              <Image
+                priority
+                src="/icons/delete.svg"
+                className={style.deleteIcon}
+                height={30}
+                width={30}
+                alt=""
+                onClick={async () => {
+                  const xx = await del(`${baseUrl}/api/users`, {
+                    id: prop("_id")(item),
+                  }).then((res) => {
+                    setCnt((val) => val - 1);
+                  });
+                }}
+              />
+            </div>
+          );
+        })(data)}
+      </article>
+      <div className={style.flex}>
+        <input value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
+        <Image
+          priority
+          src="/icons/add.svg"
+          height={30}
+          width={30}
+          alt=""
+          onClick={() => {
+            post(`${baseUrl}/api/users`, { inputVal }).then(
+              (res) => {
+                console.log("res", res);
+                setInputVal("");
+                console.log("ssssss");
+                setCnt((val) => val + 1);
+              }
+            );
+          }}
+        />
+      </div>
+    </Layout>
+  );
 }
 
-// import { useState } from "react";
-// import useSWR, { SWRConfig, useSWRConfig } from "swr";
-// import { prop, map, reject, eq, propEq } from "lodash/fp";
-// import Image from "next/image";
-// import Layout from "../components/layout";
-// import style from "./user.module.css";
-// import { del, post, read } from "../hooks/request";
+export async function getStaticProps() {
+  const baseUrl = process.env.API_BASE_URL;
+  const res = await fetch(`${baseUrl}/api/users`);
+  const users = await res.json();
 
-// const fetcher = async (url) => {
-//   const res = await fetch(url);
-//   return res.json();
-// };
-
-// export default function User({ fallback }) {
-//   const [inputVal, setInputVal] = useState("");
-//   const [cnt, setCnt] = useState(0);
-
-//   console.log("cnt==> User", cnt);
-//   const { data, error } = useSWR(
-//     ["http://localhost:3000/api/users", cnt],
-//     fetcher
-//   );
-
-//   return (
-//     <Layout>
-//       <article>
-//         {!error && !data && <div>loading</div>}
-//         {error && <div>Error fetching data</div>}
-//         {map((item) => {
-//           return (
-//             <div className={style.item} key={prop("_id")(item)}>
-//               {item.name}
-//               <Image
-//                 priority
-//                 src="/icons/delete.svg"
-//                 className={style.deleteIcon}
-//                 height={30}
-//                 width={30}
-//                 alt=""
-//                 onClick={async () => {
-//                   const xx = await del("http://localhost:3000/api/users", {
-//                     id: prop("_id")(item),
-//                   }).then((res) => {
-//                     console.log("del==>>res", res);
-//                     setCnt((val) => val - 1);
-//                   });
-//                   console.log("del del del===>", xx);
-//                 }}
-//               />
-//             </div>
-//           );
-//         })(data)}
-//       </article>
-//       <div className={style.flex}>
-//         <input value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
-//         <Image
-//           priority
-//           src="/icons/add.svg"
-//           height={30}
-//           width={30}
-//           alt=""
-//           onClick={() => {
-//             post("http://localhost:3000/api/users", { inputVal }).then(
-//               (res) => {
-//                 console.log("res", res);
-//                 setInputVal("");
-//                 console.log("ssssss");
-//                 setCnt((val) => val + 1);
-//               }
-//             );
-//           }}
-//         />
-//       </div>
-//     </Layout>
-//   );
-// }
-
-// export async function getStaticProps() {
-//   const res = await fetch("http://localhost:3000/api/users");
-//   const users = await res.json();
-
-//   return {
-//     props: {
-//       fallback: {
-//         "/api/users": users,
-//       },
-//     },
-//   };
-// }
-
-// function Users({ cnt, setCnt }) {
-//   console.log("cnt", cnt);
-//   const { data, error } = useSWR(
-//     ["http://localhost:3000/api/users", cnt],
-//     fetcher
-//   );
-//   if (error) {
-//     return <div>Error fetching data</div>;
-//   }
-
-//   if (!error && !data) return <div>loading</div>;
-
-//   return (
-//     <article>
-//       {map((item) => {
-//         return (
-//           <div className={style.item} key={prop("_id")(item)}>
-//             {item.name}
-//             <Image
-//               priority
-//               src="/icons/delete.svg"
-//               className={style.deleteIcon}
-//               height={30}
-//               width={30}
-//               alt=""
-//               onClick={() => {
-//                 del("http://localhost:3000/api/users", {
-//                   id: prop("_id")(item),
-//                 }).then((res) => {
-//                   setCnt((val) => val - 1);
-//                 });
-//               }}
-//             />
-//           </div>
-//         );
-//       })(data)}
-//     </article>
-//   );
-// }
+  return {
+    props: {
+      baseUrl,
+      fallback: {
+        "/api/users": users,
+      },
+    },
+  };
+}
