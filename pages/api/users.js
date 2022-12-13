eb002f5fd6def8e48de0763ee1d74870cbdbc6fa
hguo1212 <hguo1212@163.com>
2022-12-13 11:25:12 +0800

    test

--- pages/api/users.js
+++ pages/api/users.js
@@ -8,9 +8,10 @@ export default function get_Users(req, res) {
   console.log('method',method);
   switch (method) {
     case 'GET': 
-    kitten.find({}).then((docs)=>{
-      res.status(200).json(docs)
-    });
+    // kitten.find({}).then((docs)=>{
+    //   res.status(200).json(docs)
+    // });
+    res.status(200).json([{name:'haha'}])
     break;
     case 'POST':
         console.log('req',req)
